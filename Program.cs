d73907d26a2a5df9748d20121671e11c71cdd069
Sohail Mehmood <sohail.mehmood@worldline.com>
2022-09-01 17:13:52 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RunningTotal;
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args is null)
+        {
+            throw new ArgumentNullException(nameof(args));
+        }
+
+        var numbers = new List<int>()
+            { 107, 92, 3, 46, 5096, 666, 85 };
+
+        Console.WriteLine(numbers.Count);
+    }
+}
